c7489af87cba6d4ce05431bea404f23ccebe3b1e
epinh <epinheiro146@gmail.com>
2023-01-19 22:21:59 -0600

    completing drills

---
+++ app.js
@@ -0,0 +1,52 @@
+// name = 'Eric';
+// var name;
+// console.log(name);
+
+// setName();
+// function setName() {
+//     var name = 'Covalence';
+//     console.log(name);
+// }
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+
+// const fruits = [
+//     'apple', 'orange', 'banana'
+// ];
+
+// let favFruit;
+
+// function printFruits() {
+//     let favFruit = fruits[1];
+//     console.log(fruits[0]);
+//     function printFavFruit() {
+//         console.log(favFruit);
+//         let leastFav = fruits[2];
+//     }
+//     console.log(leastFav);
+//     printFavFruit();
+// };
+
+// printFruits();
+
+// printFavFruit();
+
+
+
+greeting();
+function greeting() {
+    console.log('Hello, Eric');
+}
+
+
+let lastString = function () {
+    alert('This is the end of the drills.');
+}
+lastString();
